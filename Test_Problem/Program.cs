8d98f5e7b2bb2c0f266dc838cd04ced9beb090e8
Denissius <denissius1987@gmail.com>
2024-03-25 15:33:39 +0300

    Добавьте файлы проекта.

---
+++ Test_Problem/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Test_Problem
+{
+    class Program
+    {
+        static void Merge1(int[] A, int p, int q, int r)
+        {
+            int wholeLength = (r - p) + 1;
+
+            int index = 0;
+            int leftindex = p;
+            int rightindex = q + 1;
+
+            int[] M = new int[wholeLength];
+
+            while (index < wholeLength)
+            {
+                if ((leftindex <= q) && (rightindex <= r))
+                {
+                    if ((A[leftindex]) <= (A[rightindex]))
+                    {
+                        M[index] = A[leftindex];
+                        leftindex++;
+                    }
+                    else
+                    {
+                        M[index] = A[rightindex];
+                        rightindex++;
+                    }
+                    index++;
+                }
+                else
+                {
+                    if (rightindex <= r)
+                    {
+                        M[index] = A[rightindex];
+                        rightindex++;
+                    }
+                    else
+                    {
+                        M[index] = A[leftindex];
+                        leftindex++;
+                    }
+                    index++;
+                }
+            }
+            for (int i = 0; i < M.Length; i++)
+            {
+                A[p + i] = M[i];
+            }
+        }
+        static int[] Sort1(int[] A, int p, int r)
+        {
+            int q = (p + r) / 2;
+            if (p < r)
+            {
+                Sort1(A, p, q);
+                Sort1(A, q + 1, r);
+                Merge1(A, p, q, r);
+            }
+            else if (p == r)
+            {
+                return A;
+            }
+            return A;
+        }
+        static void Main(string[] args)
+        {
+            int[] F = { 2, 9, 3, 4, 0 };
+
+            int[] S = Sort1(F, 0, F.Length - 1);
+
+            foreach (var item in S)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+        }
+    }
+}
